2d730acc467cb9d637d95cfb971e329775393953
AlleyChang <yifanchang0602@gmail.com>
2022-11-12 16:51:03 +0800

    feat: add user schema

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  account: {       // 員工編號
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+module.exports = mongoose.model('User', userSchema)
